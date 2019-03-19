c44f9445af2b3da2aadffe41970c99ecab5036ba
Michael S <33690140+michaelssavage@users.noreply.github.com>
2019-03-19 14:21:59 +0000

    Update ListSort.java
    Keeps on reading in numbers until a -1 is encountered and then sort the numbers and print out the numbers in sorted order.

--- Collections/Lists/ListSort.java
+++ Collections/Lists/ListSort.java
@@ -6,22 +6,21 @@ import java.util.Collections;
 public class ListSort
 {
 	public static void main(String [] args)
-    {
-	Scanner in = new Scanner(System.in);
-	List<Integer> sorted = new ArrayList<Integer>(); // Create a List of Integers
-
-	System.out.print("Enter numbers: ");
-	int num = in.nextInt();
-	while(num != -1)
 	{
-		sorted.add(num);
-	    num = in.nextInt();
-	}
+		Scanner in = new Scanner(System.in);
+		List<Integer> sorted = new ArrayList<Integer>(); // Create a List of Integers
 
-	Collections.sort(sorted);
-	System.out.print("Sorted: ");
-	for(int number : sorted)  // The Integer object is automatically converted back to an int (unbox)
-	    System.out.print(number + " ");
-	    System.out.println();
-    }
+		System.out.print("Enter numbers: ");
+		int num = in.nextInt();
+		while(num != -1)
+		{
+			sorted.add(num);
+			num = in.nextInt();
+		}
+		Collections.sort(sorted);
+		System.out.print("Sorted: ");
+		for(int number : sorted)  // The Integer object is automatically converted back to an int (unbox)
+	    		System.out.print(number + " ");
+	    		System.out.println();
+    	}
 }
